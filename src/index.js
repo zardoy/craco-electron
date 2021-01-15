c70ae3639c560c99d1cc34d43a6af38499e8bb7a
Vitaly Turovsky <vital2580@icloud.com>
2021-01-16 00:55:25 +0300

    feat: first version

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@
 const execa = require("execa");
 // assumed that the command executed from the NPM script in root package.json
 
+if (!process.env.PORT) process.env.PORT = 3500;
+if (!process.env.BROWSER) process.env.BROWSER = "none";
+
+const scriptName = process.argv[2];
+
 const cracoProcess = execa(
     "craco",
-    [process.argv[2], "--config", require.resolve("./craco.config.js")],
+    [scriptName, "--config", require.resolve("./craco.config.js")],
     { preferLocal: true }
 );
 cracoProcess.stdout.pipe(process.stdout);
