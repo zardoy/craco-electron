dd528eb52d7e19d5572f5d194dafef64b4f25efb
Vitaly Turovsky <vital2580@icloud.com>
2021-01-16 00:30:50 +0300

    testing craco

---
+++ src/index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const execa = require("execa");
+// assumed that the command executed from the NPM script in root package.json
+
+const cracoProcess = execa(
+    "craco",
+    [process.env[2], "--config", require.resolve("craco.config.js")],
+    { preferLocal: true }
+);
+cracoProcess.stdout.pipe(process.stdout);
+cracoProcess.stderr.pipe(process.stderr);
+
+cracoProcess
+    .finally(() => {
+        process.exit(process.exitCode);
+    })
