d0fef5504ab805006822e82ced4f1812d01aab90
Vitaly Turovsky <vital2580@icloud.com>
2021-01-16 09:58:02 +0300

    fix: child process stdio

--- src/index.js
+++ src/index.js
@@ -3,18 +3,25 @@
 const execa = require("execa");
 // assumed that the command executed from the NPM script in root package.json
 
-if (!process.env.PORT) process.env.PORT = 3500;
-if (!process.env.BROWSER) process.env.BROWSER = "none";
+((envDefaults) => {
+    Object.entries(envDefaults).forEach(([envVar, defaultValue]) => {
+        if (!process.env[envVar]) process.env[envVar] = defaultValue;
+    })
+})({
+    PORT: 3500,
+    BROWSER: "none"
+})
 
 const scriptName = process.argv[2];
 
 const cracoProcess = execa(
     "craco",
     [scriptName, "--config", require.resolve("./craco.config.js")],
-    { preferLocal: true }
+    {
+        preferLocal: true,
+        stdio: "inherit"
+    }
 );
-cracoProcess.stdout.pipe(process.stdout);
-cracoProcess.stderr.pipe(process.stderr);
 
 cracoProcess
     .finally(() => {
