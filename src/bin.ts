78d77530807c5f2d7c93f1afc6acb8715f8a654f
Vitaly Turovsky <vital2580@icloud.com>
2021-02-24 19:23:16 +0300

    wip: new features

--- src/bin.ts
+++ src/bin.ts
@@ -1,18 +1,25 @@
 #!/usr/bin/env node
 
 import execa from "execa";
+import fs from "fs/promises";
+import path from "path";
 
 // assumed that the command executed from the NPM script from app root
 
-((envDefaults) => {
-    Object.entries(envDefaults).forEach(([envVar, defaultValue]) => {
-        if (!process.env[envVar]) process.env[envVar] = defaultValue.toString();
+(async () => {
+    ((envDefaults) => {
+        Object.entries(envDefaults).forEach(([envVar, defaultValue]) => {
+            if (!process.env[envVar]) process.env[envVar] = defaultValue.toString();
+        });
+        fs.writeFile(
+            path.join(__dirname, "./launchConfig.json"),
+        )
+    })({
+        PORT: 3500,
+        BROWSER: "none",
+        PUBLIC_URL: "./"
     });
-})({
-    PORT: 3500,
-    BROWSER: "none",
-    PUBLIC_URL: "./"
-});
+})();
 
 const scriptName = process.argv[2];
 
